1def5e199d1fe31bd1618eb0af42dc060d9cca7b
Daniel Gathogo '20 <dgathogo@colgate.edu>
2020-01-24 12:11:11 -0500

    Math operations

--- activity1/activity1_script.r
+++ activity1/activity1_script.r
@@ -1,2 +1,21 @@
 # Activity 1
 print("hello R world")
+
+#three times three
+3*3
+#four plus seven
+4+7
+#thirty-one minus nine
+31-9
+#sixty-three divided by 8
+63/8
+
+#save 4572928 in variable a
+a <- 4572928
+
+#create a vector with five numbers
+v <- c(4,8, 12, 7,9)
+
+#multiply v with a
+v*a
+
