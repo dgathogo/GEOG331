d336d14d1853c7f1bf24ebcac470bda040ea4be8
Daniel Gathogo <dgathogo@colgate.edu>
2020-01-29 12:12:49 -0500

    first hist

--- activity2/activity2_script.r
+++ activity2/activity2_script.r
@@ -6,18 +6,6 @@ Mat.bycol[1,2] # element in row 1 col 2
 Mat.bycol[1,] # all elements in row 1
 Mat.bycol[,2] # all elements in col 2
 
-
-
-
-
-
-datW <- read.csv("q:\\Students\\dgathogo\\a02\\2011124.csv")
-str(datW)
-
-datW$dateF <- as.Date(datW$DATE, "%Y-%m-%d")
-datW$year <- as.numeric(format(datW$dateF,"%Y"))
-str(datW)
-
 #character vector
 grades <- c('a', 'b','e', 'c', 'd')
 
@@ -29,3 +17,33 @@ ages <- as.integer(c(19, 6, 38, 9, 13))
 
 #factor vector
 years <- as.factor(c('1998', '2000','1994', '1998', '1995'))
+
+
+
+datW <- read.csv("q:\\Students\\dgathogo\\a02\\2011124.csv")
+str(datW)
+
+datW$dateF <- as.Date(datW$DATE, "%Y-%m-%d")
+datW$year <- as.numeric(format(datW$dateF,"%Y"))
+str(datW)
+
+levels(datW$NAME)
+mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"], na.rm = TRUE)
+
+datW$TAVE <- datW$TMIN + ((datW$TMAX - datW$TMIN)/2)
+
+averageTemp <- aggregate(datW$TAVE, by=list(datW$NAME), FUN="mean", na.rm=TRUE)
+averageTemp
+
+colnames(averageTemp) <- c("NAME", "MAAT")
+averageTemp
+
+datW$siteN <- as.numeric(datW$NAME)
+
+hist(datW$TAVE[datW$siteN ==1], 
+     freq = FALSE,
+     main = paste(levels(datW$NAME)[1]),
+     xlab = "Average daily temperature (degrees C)",
+     ylab = "Relative frequency",
+     col = "grey50",
+     border = "white")
