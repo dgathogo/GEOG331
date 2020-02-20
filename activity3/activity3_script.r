72c67735f9f8c590060fa734606174e9c9c0db67
Daniel Gathogo '20 <dgathogo@colgate.edu>
2020-02-20 00:31:25 -0500

    question 6

--- activity3/activity3_script.r
+++ activity3/activity3_script.r
@@ -1,4 +1,4 @@
-# install.packages(c("lubridate"))
+#install.packages(c("lubridate"))
 library(lubridate)
  assert <- function(statement, err.message) {
    # evaluates if a statement is true or false 
@@ -43,9 +43,35 @@ library(lubridate)
       ylab="Air temperature (degrees C)")
   
  
+ #Question 4
+ 
+ #I'm going to make a new column to work with that indicates that I am conducting QAQC
+ #because overwriting values should be done cautiously and can lead to confusing issues.
+ #It can be particularily confusing when you are just learning R.
+ #Here I'm using the ifelse function
+ #the first argument is a logical statement to be evaluated as true or false on a vector
+ #the second argument is the value that my air.tempQ1 column will be given if the statement
+ #is true. The last value is the value that will be given to air.tempQ1 if the statement is false.
+ #In this case it is just given the air temperature value
+ datW$air.tempQ1 <- ifelse(datW$air.temperature < 0, NA, datW$air.temperature)
+ 
+ quantile(datW$air.tempQ1)
+ 
+ #look at days with really low air temperature
+ datW[datW$air.tempQ1 < 8,]  
+ 
+ #look at days with really high air temperature
+ datW[datW$air.tempQ1 > 33,]  
+ 
+ 
+ 
+ #Question 5
+ 
  #plot precipitation and lightning strikes on the same plot
  #normalize lighting strikes to match precipitation
  lightscale <- (max(datW$precipitation)/max(datW$lightning.acvitivy)) * datW$lightning.acvitivy
+ 
+ 
  #make the plot with precipitation and lightning activity marked
  #make it empty to start and add in features
  plot(datW$DD , datW$precipitation, xlab = "Day of Year", ylab = "Precipitation & lightning",
@@ -59,8 +85,18 @@ library(lubridate)
  points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
         col= "tomato3", pch=19)
  
+ 
+ #Question 6
+ 
  #filter out storms in wind and air temperature measurements
  # filter all values with lightning that coincides with rainfall greater than 2mm or only rainfall over 5 mm.    
  #create a new air temp column
  datW$air.tempQ2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
                            ifelse(datW$precipitation > 5, NA, datW$air.tempQ1))
+ 
+ 
+ datW$wind.speed2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
+                            ifelse(datW$precipitation > 5, NA, datW$wind.speed))
+ 
+ 
+ 
