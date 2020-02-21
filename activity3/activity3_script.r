4ed6943da855fb1f72b87ec84eefac4b2f527acf
dgathogo <edgetech26@gmail.com>
2020-02-21 10:56:56 -0500

    done

--- activity3/activity3_script.r
+++ activity3/activity3_script.r
@@ -12,8 +12,10 @@ library(lubridate)
  
  #evaluaate a true statement
  assert(2==2, "error: unequal values")
+ #datW <- read.csv("/Volumes/class/Geog331/Students/dgathogo/a03/bewkes_weather.csv", na.strings = c("#N/A"), skip = 3, header = F)
+ sensorInfo <- read.csv("/Volumes/class/Geog331/Students/dgathogo/a03/bewkes_weather.csv", na.strings = c("#N/A"), nrows = 2)
  
- datW <- read.csv("q:\\Students\\dgathogo\\a03\\bewkes_weather.csv", na.strings = c("#N/A"), skip = 3, header = F)
+ #datW <- read.csv("q:\\Students\\dgathogo\\a03\\bewkes_weather.csv", na.strings = c("#N/A"), skip = 3, header = F)
  #print(datW[1,])
  
  sensorInfo <- read.csv("q:\\Students\\dgathogo\\a03\\bewkes_weather.csv", na.strings = c("#N/A"), nrows = 2)
@@ -85,6 +87,7 @@ library(lubridate)
  points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
         col= "tomato3", pch=19)
  
+ assert(length(lightscale) == length(datW$DD), "Unequal lengths")
  
  #Question 6
  
@@ -97,6 +100,39 @@ library(lubridate)
  
  datW$wind.speed2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
                             ifelse(datW$precipitation > 5, NA, datW$wind.speed))
+ 
+ for (i in 1 : length(datW$precipitation)) {
+    assert(!(datW$precipitation[i]  >= 2 & datW$lightning.acvitivy[i] >0 & is.na(datW$wind.speed2[i])), "Unfiltered wind speed data")
+    assert(!(datW$precipitation[i] > 5 & is.na(datW$wind.speed2[i])), "Unfiltered wind speed data") 
+ }
+ 
+ plot(datW$DD, datW$wind.speed2, xlab = "Day of Year", ylab = "Wind speed", type = 'b')
+ plot(datW$DD, datW$wind.speed, xlab = "Day of Year", ylab = "Wind speed", type = 'b')
+ 
+ #Question 7
+ 
+ plot(datW$DD, datW$precipitation, xlab = "Day of Year", ylab = "Precipitation")
+ plot(datW$DD, datW$soil.moisture, xlab = "Day of Year", ylab = "Soil Moisture")
+ plot(datW$DD, datW$soil.temp, xlab = "Day of Year", ylab = "Soil Temperature")
+ plot(datW$DD, datW$air.temperature, xlab = "Day of Year", ylab = "Air Temperature")
+ 
+ 
+ #Question 8
+ 
+ avgAirTemp <- mean(datW$air.temperature, na.rm = T)
+ avgWindSpeed <- mean(datW$wind.speed, na.rm = T)
+ avgSoilMoisture <- mean(datW$soil.moisture, na.rm = T)
+ avgSoilTemp <- mean(datW$soil.temp, na.rm = T)
+ totalPrecp <- sum(datW$precipitation)
+
+ #Question 9 
+ 
+ plot(datW$DD, datW$precipitation, xlab = "Day of Year", ylab = "Precipitation")
+ plot(datW$DD, datW$soil.moisture, xlab = "Day of Year", ylab = "Soil Moisture")
+ plot(datW$DD, datW$soil.temp, xlab = "Day of Year", ylab = "Soil Temperature")
+ plot(datW$DD, datW$air.temperature, xlab = "Day of Year", ylab = "Air Temperature")
+ 
+ 
  
  
  
