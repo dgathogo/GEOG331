9edb0d5747c82bab10895fa571a0ff0cbe07ce5d
Daniel Gathogo <dgathogo@colgate.edu>
2020-02-28 11:28:34 -0500

    activity 5

---
+++ activity5/activity5_script.r
@@ -0,0 +1,57 @@
+library(lubridate)
+#read in streamflow data
+datH <- read.csv("q:\\Students\\dgathogo\\a05\\stream_flow_data.csv",
+                 na.strings = c("Eqp"))
+#read in precipitation data
+#hourly precipitation is in mm
+datP <- read.csv("q:\\Students\\dgathogo\\a05\\2049867.csv")                          
+
+#only use most reliable measurements
+datD <- datH[datH$discharge.flag == "A",]
+
+#### define time for streamflow #####
+#convert date and time
+datesD <- as.Date(datD$date, "%m/%d/%Y")
+#get day of year
+datD$doy <- yday(datesD)
+#calculate year
+datD$year <- year(datesD)
+#define time
+timesD <- hm(datD$time)
+
+#### define time for precipitation #####    
+dateP <- ymd_hm(datP$DATE)
+#get day of year
+datP$doy <- yday(dateP)
+#get year 
+datP$year <- year(dateP)
+
+
+# Question 2
+#### get decimal formats #####
+#convert time from a string to a more usable format
+#with a decimal hour
+datD$hour <- hour(timesD ) + (minute(timesD )/60)
+#get full decimal time
+datD$decDay <- datD$doy + (datD$hour/24)
+#calculate a decimal year, but account for leap year
+datD$decYear <- ifelse(leap_year(datD$year),datD$year + ((datD$decDay-1)/366),
+                       datD$year + ((datD$decDay -1)/365))
+#calculate times for datP                       
+datP$hour <- hour(dateP ) + (minute(dateP )/60)
+#get full decimal time
+datP$decDay <- datP$doy + (datP$hour/24)
+#calculate a decimal year, but account for leap year
+datP$decYear <- ifelse(leap_year(datP$year),datP$year + (datP$decDay/366),
+                       datP$year + (datP$decDay/365)) 
+
+
+#plot discharge
+plot(datD$decYear, datD$discharge, type="l", xlab="Year", ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+# Question 3
+head(datH)
+head(datP)
+
+#Question 4
+
+help("expression")
