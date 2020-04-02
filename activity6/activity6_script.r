041795aed8b6816f8c8b0d55235f635bb3cae915
dgathogo <edgetech26@gmail.com>
2020-04-01 22:46:58 -0500

    activity 6

--- activity6/activity6_script.r
+++ activity6/activity6_script.r
@@ -7,13 +7,13 @@ library(plyr)
 
 #read in shapefiles
 #readOGR in rgdal does this
-g1966 <- readOGR("Y:\\Students\\dgathogo\\a06\\GNPglaciers\\GNPglaciers_1966.shp")
+g1966 <- readOGR("/Users/dgathogo/github/envdatascience/a06/GNPglaciers/GNPglaciers_1966.shp")
 
-g1998 <- readOGR("Y:\\Students\\dgathogo\\a06\\GNPglaciers\\GNPglaciers_1998.shp")
+g1998 <- readOGR("/Users/dgathogo/github/envdatascience/a06/GNPglaciers/GNPglaciers_1998.shp")
 
-g2005 <- readOGR("Y:\\Students\\dgathogo\\a06\\GNPglaciers\\GNPglaciers_2005.shp")
+g2005 <- readOGR("/Users/dgathogo/github/envdatascience/a06/GNPglaciers/GNPglaciers_2005.shp")
 
-g2015 <- readOGR("Y:\\Students\\dgathogo\\a06\\GNPglaciers\\GNPglaciers_2015.shp")
+g2015 <- readOGR("/Users/dgathogo/github/envdatascience/a06/GNPglaciers/GNPglaciers_2015.shp")
 
 str(g1966)
 spplot(g1966, "GLACNAME")
@@ -26,9 +26,9 @@ g2015@data$GLACNAME <- ifelse(g2015@data$GLACNAME == "North Swiftcurrent Glacier
                                         as.character(g2015@data$GLACNAME)))
 
 
-redL <- raster("Y:\\Students\\dgathogo\\a06\\glacier_09_05_14\\l08_red.tif")
-greenL <- raster("Y:\\Students\\dgathogo\\a06\\glacier_09_05_14\\l08_green.tif")
-blueL <- raster("Y:\\Students\\dgathogo\\a06\\glacier_09_05_14\\l08_blue.tif")
+redL <- raster("/Users/dgathogo/github/envdatascience/a06/glacier_09_05_14/l08_red.tif")
+greenL <- raster("/Users/dgathogo/github/envdatascience/a06/glacier_09_05_14/l08_green.tif")
+blueL <- raster("/Users/dgathogo/github/envdatascience/a06/glacier_09_05_14/l08_blue.tif")
 
 rgbL <- brick(redL, greenL, blueL)
 
@@ -56,13 +56,109 @@ ndviYear <- seq(2003,2016)
 #read all files into a list
 NDVIraster <- list() 
 for(i in 1:length(ndviYear)){
-  NDVIraster[[i]] <- raster(paste0("Y:\\Students\\dgathogo\\a06\\NDVI\\NDVI_",ndviYear[i],".tif"))
+  NDVIraster[[i]] <- raster(paste0("/Users/dgathogo/github/envdatascience/a06/NDVI/NDVI_",ndviYear[i],".tif"))
   
 }
 
 str(NDVIraster[[1]])
 
+
+# Question 4
+
 par(mai=c(1,1,1,1))
 par(mfrow=c(1,2))
 plot(g1966,axes=T)
 plot(NDVIraster[[1]], axes=T)
+
+g1966p <- spTransform(g1966,NDVIraster[[1]]@crs)
+g1998p <- spTransform(g1998,NDVIraster[[1]]@crs)
+g2005p <- spTransform(g2005,NDVIraster[[1]]@crs)
+g2015p <- spTransform(g2015,NDVIraster[[1]]@crs)
+
+par(mai=c(1,1,1,1))
+par(mfrow=c(1,2))
+plot(g2015p, border=T, axes=F)
+plot(NDVIraster[[13]], axes=F)
+
+# Question 5
+
+g1966p@data$a1966m.sq <- area(g1966p)
+g1998p@data$a1998m.sq <- area(g1998p)
+g2005p@data$a2005m.sq <- area(g2005p)
+g2015p@data$a2015m.sq <- area(g2015p)
+
+gAllp1 <- join(g1966p@data,g1998p@data, by="GLACNAME", type="full")
+gAllp2 <- join(gAllp1,g2005p@data, by="GLACNAME", type="full")
+gAll <- join(gAllp2,g2015p@data, by="GLACNAME", type="full")
+plot(c(1966,1998,2005,2015), 
+c(gAll$a1966m.sq[1],gAll$a1998m.sq[1], gAll$a2005m.sq[1],gAll$a2015m.sq[1]),
+type="b", 
+pch=19, col=rgb(0.5,0.5,0.5,0.5), xlim= c(1965,2016),
+ylim=c(0,2000000),
+ylab="Area of glacier (meters squared)",
+xlab="Year")
+
+for(i in 2:39){
+  points(c(1966,1998,2005,2015), 
+         c(gAll$a1966m.sq[i],gAll$a1998m.sq[i], gAll$a2005m.sq[i],gAll$a2015m.sq[i]),
+         type="b", 
+         pch=19, col=rgb(0.5,0.5,0.5,0.5))
+  
+} 
+g1966.total = sum(g1966p@data$a1966m.sq)
+g2015.total = sum(g2015p@data$a2015m.sq)
+percentage = (g1966.total - g2015.total)/ g1966.total * 100
+
+
+# spplot(gDifference(g1966p, g2015p)) 
+
+# Question 6
+
+# diffPoly <- gDifference(g1966p, g2015p)
+# plot(diffPoly)
+
+#plot with NDVI
+plot(NDVIraster[[13]], axes=FALSE, box=FALSE)
+# plot(diffPoly,col="black", border=NA,add=TRUE)
+
+
+# Question 7
+
+#extract NDVI values
+NDVIdiff <- list()
+meanDiff <- numeric(0)
+#loop through all NDVI years
+for(i in 1:length(ndviYear)){
+  #get raster values in the difference polygon
+  # NDVIdiff[[i]] <- extract(NDVIraster[[i]],diffPoly)[[1]]
+  #calculate the mean of the NDVI values
+  meanDiff[i] <- mean(NDVIdiff[[i]], na.rm=TRUE)
+}
+
+plot(ndviYear, meanDiff, type="b",
+     xlab= "Year",
+     ylab="Average NDVI (unitless)",
+     pch=19)
+
+#designate that NDVIraster list is a stack
+NDVIstack <- stack(NDVIraster)
+#set up lm function to apply to every cell
+#where x is the value of a cell
+#need to first skip NA values (like lakes)
+#if NA is missing in first raster, it is missing in all
+#so we can tell R to assign an NA rather than fitting the function
+timeT <- ndviYear
+fun <- function(x) {
+  if(is.na(x[1])){
+    NA}else{
+      #fit a regression and extract a slope
+      lm(x ~ timeT)$coefficients[2] }}
+#apply the slope function to the rasters
+NDVIfit <- calc(NDVIstack,fun)
+#plot the change in NDVI
+plot(NDVIfit, axes=FALSE)
+
+
+
+
+
