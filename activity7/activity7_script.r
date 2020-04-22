539d82a9711c9a0303f5f34ba49c87e5847636d3
dgathogo <edgetech26@gmail.com>
2020-04-21 20:40:24 -0500

    start activity 7

---
+++ activity7/activity7_script.r
@@ -0,0 +1,33 @@
+#install.packages(c('raster','sp','rgdal', 'rgeos','mapview','caret','randomForest','nnet'))
+library(raster)
+library(sp)
+library(rgdal)
+library(rgeos)
+library(mapview)
+library(caret)
+library(randomForest)
+library(nnet)
+
+#set up directory for oneida data folder
+dirR <- '~/github/envdatascience/oneida'
+#read in Sentinel data
+
+rdatB2 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B02_20m.tif"))
+rdatB3 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B03_20m.tif"))
+rdatB4 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B04_20m.tif"))
+rdatB5 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B05_20m.tif"))
+rdatB6 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B06_20m.tif"))
+rdatB7 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B07_20m.tif"))
+rdatB8 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B08_20m.tif"))
+rdatB11 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B11_20m.tif"))
+rdatB12 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B12_20m.tif"))
+clouds <- raster(paste0(dirR,"/sentinel/MSK_CLDPRB_20m.tif"))
+
+#read in validation data
+#here verbose=FALSE hiddes
+algae <- readOGR(paste0(dirR,"/Oneida/algae.shp"), verbose=FALSE)
+agri <- readOGR(paste0(dirR,"/Oneida/agriculture.shp"), verbose=FALSE)
+built <- readOGR(paste0(dirR,"/Oneida/built.shp"), verbose=FALSE)
+forest <- readOGR(paste0(dirR,"/Oneida/forest.shp"), verbose=FALSE)
+water <- readOGR(paste0(dirR,"/Oneida/water.shp"), verbose=FALSE)
+wetlands <- readOGR(paste0(dirR,"/Oneida/wetlands.shp"), verbose=FALSE)
